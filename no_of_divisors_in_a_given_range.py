da798d808058fde5e09c134e0c4cc4e7f60ea5e2
Shubhangini Tripathy <shubhanginitripathy@gmail.com>
2017-11-04 20:59:57 +0530

    solve geeks for geeks problem

---
+++ no_of_divisors_in_a_given_range.py
@@ -0,0 +1,8 @@
+T = int(input())
+for i in range(T):
+    m, n, a, b = map(int, input().split())
+    count=0
+    for num in range(m, n + 1):
+        if num % a == 0 or num % b == 0:
+            count += 1
+    print(count)
