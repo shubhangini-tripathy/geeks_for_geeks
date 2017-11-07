083d8fdd3ac58cb0ceebf1bbf72f07c3c6efdf70
Shubhangini Tripathy <shubhanginitripathy@gmail.com>
2017-11-07 20:31:28 +0530

    solve geeks for geeks problem

---
+++ print_no_of_smaller_elements.py
@@ -0,0 +1,14 @@
+N=int(input())
+for i in range(N):
+    T= int(input())
+    c = [int(x) for x in input().split()]
+    Q= int(input())
+    count=0
+    for j in range(len(c)):
+        if c[j]<=Q:
+            count += 1
+    print(count)
+
+        
+    
+    
