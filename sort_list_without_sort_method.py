2d2ec7f657a8f800d64b7a44cb055fec12a71d15
Shubhangini Tripathy <shubhanginitripathy@gmail.com>
2017-11-10 21:06:32 +0530

    solve problem in O(n) time

--- sort_list_without_sort_method.py
+++ sort_list_without_sort_method.py
@@ -1,6 +1,18 @@
+# A = [0, 1, 1, 0, 0, 1, 0, 1, 1, 1, 0, 0, 0]
+# for i in range(len(A)):
+#     for j in range(len(A) - 1):
+#         if A[j] > A[j + 1]:
+#             A[j + 1], A[j] = A[j], A[j + 1]
+# print(A)
+
+
 A = [0, 1, 1, 0, 0, 1, 0, 1, 1, 1, 0, 0, 0]
-for i in range(len(A)):
-    for j in range(len(A) - 1):
-        if A[j] > A[j + 1]:
-            A[j + 1], A[j] = A[j], A[j + 1]
+i, j = 0, len(A) - 1
+while i < j:
+    if A[i] == 1:
+        while A[j] == 1:
+            j -= 1
+        if i < j:
+            A[i], A[j] = A[j], A[i]
+    i += 1
 print(A)
