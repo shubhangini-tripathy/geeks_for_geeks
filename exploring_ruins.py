a347d28ff4fb4d508a9a7fe7be50f36bab421ff3
Shubhangini Tripathy <shubhanginitripathy@gmail.com>
2017-11-17 00:50:20 +0530

    Explore ruins

---
+++ exploring_ruins.py
@@ -0,0 +1,16 @@
+s = input()
+A = list(s)
+for i in range(len(A)):
+    if A[i] == "?":
+        if A[i-1] == "a":
+            A[i] = "b"
+        elif i+1 < len(A) and A[i+1] == "a":
+            A[i] = "b"
+        else:
+            A[i] = "a"
+print(''.join(A))
+
+
+    
+
+        
