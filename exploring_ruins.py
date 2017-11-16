432a9d85f7de6554b6dd3feef73285329f3ecb01
Shubhangini Tripathy <shubhanginitripathy@gmail.com>
2017-11-17 01:01:26 +0530

    Solve one failed test case

--- exploring_ruins.py
+++ exploring_ruins.py
@@ -2,15 +2,10 @@ s = input()
 A = list(s)
 for i in range(len(A)):
     if A[i] == "?":
-        if A[i-1] == "a":
+        if i > 0 and A[i - 1] == "a":
             A[i] = "b"
-        elif i+1 < len(A) and A[i+1] == "a":
+        elif i + 1 < len(A) and A[i + 1] == "a":
             A[i] = "b"
         else:
             A[i] = "a"
 print(''.join(A))
-
-
-    
-
-        
