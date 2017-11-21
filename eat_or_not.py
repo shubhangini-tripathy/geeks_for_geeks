921a3b5ee90ea17a93bca45df82cbb2a690dc534
Shubhangini Tripathy <shubhanginitripathy@gmail.com>
2017-11-21 20:31:56 +0530

    solve hackerearth problem

--- eat_or_not.py
+++ eat_or_not.py
@@ -1,12 +1,9 @@
-t = int(input())
-for i in range(t):
-    str1 = input()
-    result = []
-    result.append(str1[0])
-    for j in range(1,len(str1)):
-        if str1[j] not in result:
-            result.append(str1[j])
-    print(''.join(result))
-
-
-    
+N = int(input())
+for i in range(N):
+    A = input()
+    S = "SUVO"
+    T = "SUVOJIT"
+    count1 = A.count(T)
+    count = A.count(S)
+    count = count - count1    
+    print("SUVO = {0}, SUVOJIT = {1}".format(count, count1))
